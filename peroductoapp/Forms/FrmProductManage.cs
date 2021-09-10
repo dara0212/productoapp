d5c1534156da8c149613d24ee85203fe42a6ecc1
dara0212 <90067132+dara0212@users.noreply.github.com>
2021-09-09 20:11:30 -0600

    Iniciando proyecto

--- peroductoapp/Forms/FrmProductManage.cs
+++ peroductoapp/Forms/FrmProductManage.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace peroductoapp
+namespace peroductoapp.Forms
 {
-    public partial class Form1 : Form
+    public partial class FrmProductManage : Form
     {
-        public Form1()
+        public FrmProductManage()
         {
             InitializeComponent();
         }
